169eabaab86081884273c6297a4e1c26b67416b7
박승채 <@DESKTOP-P53MBUO>
2022-01-10 14:39:40 +0900

    Completion Circle

---
+++ src/test/this2/Circle.java
@@ -0,0 +1,45 @@
+package test.this2;
+
+public class Circle {
+	private int r;
+	private double circum, area;
+	
+	
+	
+	public int getR() {
+		return r;
+	}
+
+
+
+	public double getCircum() {
+		return circum;
+	}
+
+
+
+	public double getArea() {
+		return area;
+	}
+
+
+	public Circle(int r) {
+		this.r = r;
+	}
+	
+	//반지름이 r인 원의 둘레를 계산하는 메소드
+	public void calcCircum() {
+		circum = 2 * Math.PI * r;
+	}
+	
+	//반지름이 r인 원의 면적 계산하는 메소드
+	public void calcArea() {
+		area = Math.PI * Math.pow(r, 2);
+	}
+	
+	public void showInfo() {
+		calcCircum();
+		calcArea();
+		RunCircle.print(this);
+	}
+}
